ad5a8d2060fa462aa5f482a2af325c63e51b0b97
TivonteBrown <120049857+TivonteBrown@users.noreply.github.com>
2024-04-17 12:40:10 -0400

    ConnectionFactory-application.properties start added

---
+++ E-Commerce App/src/main/java/com/revature/eCommerce/utils/ConnectionFactory.java
@@ -0,0 +1,13 @@
+import java.io.InputStream;
+import java.sql.Connections;
+import java.sql.DriverManager;
+import java.sql.SQLException
+import java.util.Properties;
+
+
+public class ConnectionFactory{
+    private static  ConnectionFactory instance;
+    private Connection conn;
+
+    private ConnectionFactory getInstance
+}
