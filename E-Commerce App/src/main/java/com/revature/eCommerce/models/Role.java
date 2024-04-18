28353f1c66fa2fac57d75032a4413d5c81dd3ef0
TivonteBrown <120049857+TivonteBrown@users.noreply.github.com>
2024-04-17 20:49:47 -0400

    Role model

---
+++ E-Commerce App/src/main/java/com/revature/eCommerce/models/Role.java
@@ -0,0 +1,22 @@
+package com.revature.eCommerce.models;
+
+public class Role {
+
+
+    private String roleName;
+
+    public Role(){}
+
+    public Role(String roleName) {
+        this.roleID = roleID;
+    }
+
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
