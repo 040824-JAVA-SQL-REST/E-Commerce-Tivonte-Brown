42a27ec525e4af2fc24e9da88225819527bd1b69
TivonteBrown <120049857+TivonteBrown@users.noreply.github.com>
2024-04-19 11:27:29 -0400

    App class edit - Tivonte

--- E-Commerce App/src/main/java/com/revature/eCommerce/App.java
+++ E-Commerce App/src/main/java/com/revature/eCommerce/App.java
@@ -1,13 +1,16 @@
-package com.mycompany.app;
+package com.revature.eCommerce;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+import com.revature.eCommerce.services.RouterService;
+import com.revature.eCommerce.services.UserService;
+
+public class App {
+    public static void main( String[] args )  {
+        Scanner scan = new Scanner(System.in);
+
+        new RouterService(new UserService(), scan)
+             .navigate("/start").startInterface();
+
+        scan.close();
     }
 }
