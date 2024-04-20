473d413b437a57df019adab838c4ce3e862af537
TivonteBrown <120049857+TivonteBrown@users.noreply.github.com>
2024-04-20 18:54:57 -0400

    update userservice

--- E-Commerce App/src/main/java/com/revature/eCommerce/services/UserService.java
+++ E-Commerce App/src/main/java/com/revature/eCommerce/services/UserService.java
@@ -14,6 +14,6 @@ public class UserService {
 
     public boolean isValidPassword(String password){
         //Minimum eight characters, at least one letter, one number and one special character:
-        return password.matches("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$")
+        return password.matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$")
     }
 }
