9fd73d1b130ae478d4f49b02461579da9ac41877
TivonteBrown <TivonteBrown@Gmail.com>
2024-04-24 14:21:09 -0400

    Products model

---
+++ src/main/java/com/revature/eCommerce/models/Products.java
@@ -0,0 +1,43 @@
+package com.revature.eCommerce.models;
+
+public class Products {
+
+    private String productID;
+    private int productValue;
+    private String seller;
+
+    public Products (){}
+
+    public Products(String productID, int productValue, String seller) {
+        this.productID = productID;
+        this.seller = seller;
+        this.productValue = productValue;
+        this.orderHistory = orderHistory;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
+    public int getProductValue() {
+        return productValue;
+    }
+
+    public void setProductValue(int productValue) {
+        this.productValue = productValue;
+    }
+
+
+}
