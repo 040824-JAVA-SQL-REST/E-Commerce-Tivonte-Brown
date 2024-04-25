751665511eafe88ca1c4053c54d52f6955eca050
TivonteBrown <TivonteBrown@Gmail.com>
2024-04-24 22:31:39 -0400

    UserController register update

--- src/main/java/com/revature/eCommerce/App.java
+++ src/main/java/com/revature/eCommerce/App.java
@@ -1,9 +1,5 @@
 package com.revature.eCommerce;
 
-import java.util.Scanner;
-import java.sql.SQLException;
-import java.io.IOException;
-
 import com.revature.eCommerce.services.RouterService;
 import com.revature.eCommerce.services.UserService;
 import com.revature.eCommerce.services.RoleService;
@@ -11,31 +7,33 @@ import com.revature.eCommerce.utils.ConnectionFactory;
 import com.revature.eCommerce.dao.UserDao;
 import com.revature.eCommerce.dao.RoleDao;
 import com.revature.eCommerce.models.User;
+import com.revature.eCommerce.Controllers.UserController;
 
+import java.util.Scanner;
+import java.sql.SQLException;
+import java.io.IOException;
+import io.javalin.Javalin;
+
+import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class App {
     public static void main( String[] args ) throws SQLException, IOException  {
-        Scanner scan = new Scanner(System.in);
-        User session = new User();
+            App app = new App();
+            UserController userController = new UserController();
 
-        System.out.println(ConnectionFactory.getInstance().getConnection());
-        scan.nextLine();
+            Javalin.create(config -> {
+                config.router.apiBuilder(()-> {
+                    path("/user", () -> {
+                        post("/register", userController::register);
+                    });
+                });
+            }).start(7070);
 
-/*         new RouterService(new UserService(new UserDao()), scan, session)
-             .navigate("/start")
-
-             .startInterface(); */
-
-        new RouterService(new App().getUserService(), scan, session)
-             .navigate("/start")
-             .startInterface();
-
-        scan.close();
     }
 
-    private UserService getUserService(){
-        return new UserService(new RoleService(new RoleDao()), new UserDao());
-    }
+        private UserService getUserService(){
+            return new UserService(new RoleService(new RoleDao()), new UserDao());
+        }
 
 }
 
--- src/main/java/com/revature/eCommerce/Controllers/UserController.java
+++ src/main/java/com/revature/eCommerce/Controllers/UserController.java
@@ -1,5 +1,10 @@
 package com.revature.eCommerce.Controllers;
 
+import io.javalin.http.Context;
+
 public class UserController {
+    public void register(Context ctx){
+        ctx.result("User");
+    }
 
 }
